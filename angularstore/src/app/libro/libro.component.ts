71147984a070e103ab3d36b51056520f8fd4e04e
edi <edixon_guerrero96@hotmail.com>
2019-12-20 18:01:01 -0500

    firstjsonlecture

--- angularstore/src/app/libro/libro.component.ts
+++ angularstore/src/app/libro/libro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-libro',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./libro.component.css']
 })
 export class LibroComponent implements OnInit {
+  
+  url = `http://echo.jsontest.com/key/value/one/two`;
+  items = []
+  
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+    });
+   }
 
-  constructor() { }
+ 
 
   ngOnInit() {
+   
   }
 
 }
