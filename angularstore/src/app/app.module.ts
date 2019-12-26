43764ae690de818081f8f44492aa9d597d9668c8
edi <edixon_guerrero96@hotmail.com>
2019-12-26 15:08:01 -0500

    infinitescrollexample

--- angularstore/src/app/app.module.ts
+++ angularstore/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LibroComponent } from './libro/libro.component';
 import { HttpClientModule } from '@angular/common/http'; 
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxSpinnerModule } from "ngx-spinner";
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +16,14 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,FormsModule,Ng2SearchPipeModule
+    HttpClientModule,FormsModule,Ng2SearchPipeModule,InfiniteScrollModule,NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+ 
+  
+}
--- angularstore/src/app/libro/libro.component.ts
+++ angularstore/src/app/libro/libro.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OnInit, ViewChild, ElementRef } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { share, finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-libro',
   templateUrl: './libro.component.html',
@@ -11,32 +12,78 @@ import { OnInit, ViewChild, ElementRef } from '@angular/core';
 export class LibroComponent implements OnInit {
   @ViewChild('nameInput') nameInput: ElementRef;
   
-  url = `/api/courses?orderBy=popularity+desc&expand=provider&limit=24&profession=&subjectAreaCode=&state=&provider=&name=`;
+  url = `/api/courses?limit=24&offset=24&orderBy=popularity%20desc&expand=provider&customPageId=0"`;
 datos=[];
 encontrado=[];
 mensa='';
 searchText;
+sum = 100;
+direction = '';
+array = [];
+public news: Array<any> = [];
+
+  private currentPage = 1;
+
+  private request$: Observable<any>;
 
   constructor(private http: HttpClient) {
-   
+  
     this.http.get(this.url).toPromise().then(data => {
       
       
-for(let key in data.items)
-this.datos.push(data.items[key]);
+for(let key in data)
+this.datos.push(data[key]);
     });
 
-    
+    console.log(this.datos);
     
    }
 
  
 
   ngOnInit() {
-    
+    this.getNews(this.currentPage)
+    .pipe(finalize(() => this.onFinalize()))
+    .subscribe((news) => {
+      this.currentPage++;
+      this.news = this.news.concat(news);
+    });
+  }
+  onScrollDown () {
+    console.log('scrolled down!!');
+this.mensa="abajo";
+this.getNews(this.currentPage)
+      .pipe(finalize(() => this.onFinalize()))
+      .subscribe((news) => {
+        this.currentPage++;
+        this.news = this.news.concat(news);
+      });
+   
+  }
+  
+  onUp() {
+    console.log('scrolled up!');
+    this.mensa="arriba";
+    this.getNews(this.currentPage)
+    .pipe(finalize(() => this.onFinalize()))
+    .subscribe((news) => {
+      this.currentPage++;
+      this.news = news.concat(this.news);
+    });
+
+  }
+  private getNews(page: number = 1): Observable<any> {
+    if (this.request$) {
+      return this.request$;
+    } else {
+      this.request$ = this.http.get(`https://node-hnapi.herokuapp.com/news?page=${page}`).pipe(share());
+      return this.request$;
+    }
   }
 
-    
+  private onFinalize(): void {
+    this.request$ = null;
+  }
    
   }
   
